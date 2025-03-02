fadc424b49156080597901dc72a2ea9361aea891
vansh162 <vanshthakkar1622005@gmail.com>
2025-03-02 14:18:57 +0530

    Python Practice

---
+++ Day-1/first_script.py
@@ -0,0 +1,3 @@
+print("Name: Vansh Thakkar")
+print("Age: 20")
+print("Collage: L.J. Collage")
---
+++ Day-1/pattern.py
@@ -0,0 +1,6 @@
+a = range(1,4)
+b = range(2,0,-1)
+for i in a:
+    print("*"*i)
+for i in b:
+    print("*"*i)
